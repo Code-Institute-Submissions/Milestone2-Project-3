d1310b9a943863b03dfabf25363d1ef74fdaf4b8
martinjohnson26 <martin.johnson26@outlook.com>
2020-09-29 14:41:44 +0000

    add correct colours to card elements by using mouse enter/leave event

--- assets/js/fixtures.js
+++ assets/js/fixtures.js
@@ -4,13 +4,21 @@ $(document).ready(function () {
 
 $(".fixtures-btn").click(function(){
     $(this).siblings('p').removeClass('invisible').toggle("fast");
-       
+    let cardColour = $(this).attr('data-colour') ;
+    /*$(this).addClass(cardColour)*/
+    $(this).parents(".card").addClass(cardColour)
 });
     
 
- $(".card").mouseenter(function(){
-    $(this).addClass('testbackground')
+$(".card").mouseenter(function(){
+    $(this).find('p').removeClass('invisible').toggle("fast");
+    let cardColour = $(this).find('a').attr('data-colour') ;
+   $(this).addClass(cardColour)
+    
 });
+
 $(".card").mouseleave(function(){
-    $(".card").removeClass('testbackground')
+    $(this).find('p').addClass('invisible').toggle("fast");
+    let cardColour = $(this).find('a').attr('data-colour') ;
+    $(this).removeClass(cardColour)
 }); 
